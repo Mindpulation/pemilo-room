2db2b65135a814184f34ecffcb38b3b9bc6d066e
dev-zam <zamsyh.work@gmail.com>
2020-10-15 12:10:11 +0700

    create index.js for handle all router

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const conn = require('./env/index');
+const insertAnggota = require('./view/insertAnggota');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/anggota', insertAnggota);
+
+app.all("*", (req, res) => {
+    res.send({message: "The router or endpoint you entered was not found"});
+});
+
+const server = http.createServer(app);
+server.listen(conn.PORT);
