92e8cf2437dd57ad5c065ec95e280968523af6ee
Reza Andriansyah <technoboth@gmail.com>
2020-11-25 23:43:35 +0700

    fix prob

--- controller/mongo.js
+++ controller/mongo.js
@@ -38,17 +38,17 @@ const findAnggota = async (req, res) => {
 
 const countAnggota = async (req, res) => {
     const ress = await getCount(con, {codeRoom:req.body.codeRoom});    
-    res.send(ress).status(200);
+    res.send(ress);
 }
 
 const countRoom = async (req, res) => {
     const ress =  await getCount(con2, {emailAdmin : req.body.emailAdmin});
-    res.send(ress).status(200);
+    res.send(ress);
 }
 
 const countRoomWithSta = async (req, res) => {
     const ress = await getCount(con2, {emailAdmin : req.body.emailAdmin, status:req.body.sta});
-    res.send(ress).status(200);
+    res.send(ress);
 }
 
 const updateStatusAnggota = async (req, res) => {
