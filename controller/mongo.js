86b6cfd7119be6766f703858106f8d598a2aab68
Indra <me@aboutindra.com>
2020-11-27 01:18:26 +0700

    Debugging

--- controller/mongo.js
+++ controller/mongo.js
@@ -131,49 +131,30 @@ const processExcel = async (req, res) => {
     const upload = multer({storage: storage, limits : {fileSize : 250000000}}).single("excel_file");
 
     upload(req, res, (err) => {
+        console.log(req.body.code_room)
 
         if(err) {
             res.status(400).send("Something went wrong!\nmsg : "+ err);
         }
-        var XLSX = require('xlsx');
-        var workbook = XLSX.readFile(req.file.path);
-        var sheet_name_list = workbook.SheetNames;
-        var data = [];
-        sheet_name_list.forEach(function(y) {
-            var worksheet = workbook.Sheets[y];
-            var headers = {};
-            for(z in worksheet) {
-                if(z[0] === '!') continue;
-                //parse out the column, row, and value
-                var tt = 0;
-                for (var i = 0; i < z.length; i++) {
-                    if (!isNaN(z[i])) {
-                        tt = i;
-                        break;
-                    }
-                };
-                var col = z.substring(0,tt);
-                var row = parseInt(z.substring(tt));
-                var value = worksheet[z].v;
-
-                //store header names
-                if(row == 1 && value) {
-                    headers[col] = value;
-                    continue;
-                }
-
-                if(!data[row]) data[row]={};
-                data[row][headers[col]] = value;
+        var result = excelToJson({
+            sourceFile: req.file.path,
+            header: {
+                rows: 1
+            },
+            columnToKey: {
+                '*': '{{columnHeader}}'
             }
-            //drop those first two rows which are empty
-            data.shift();
-            data.shift();
-            // console.log(data);
         });
+        var keys = Object.keys(result);
+        console.log(keys);
+        const hasil = [];
+        for(let i = 0; i < keys.length; i++){
+            hasil.push(result[keys[i]])
+        }
         const data2 = { status:false, codeRoom: req.body.code_room }
-        data.map(data => Object.assign(data, data2));
-        console.log("================================\n",data);
-        const resultSaveMany = saveMany(con, data)
+        var finalData = [].concat.apply([], hasil);
+        finalData.map(data => Object.assign(data, data2));
+        const resultSaveMany = saveMany(con, finalData);
         res.send({"res" : resultSaveMany});
     });
 }
